179ee135c324242cfc91fc3a04bb87ea0a8140fb
PoonamMangotra <FRBAO@ascensia.net>
2021-05-22 23:28:32 +0530

    Control UI

--- app/src/main/java/com/example/basicandroidapplication/MainActivity.kt
+++ app/src/main/java/com/example/basicandroidapplication/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.basicandroidapplication
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText.addTextChangedListener {
+            if (toggleButton.isChecked)
+                textView.text = it.toString()
+        }
+
+        boldCB.setOnCheckedChangeListener { _, isChecked ->
+            when(isChecked){
+                true -> { textView.typeface = Typeface.DEFAULT_BOLD}
+                false -> {textView.typeface = Typeface.DEFAULT}
+            }
+        }
+
+        italicCB.setOnCheckedChangeListener { _, isChecked ->
+            when(isChecked){
+                true -> { textView.setTypeface(null, Typeface.ITALIC) }
+                false -> {textView.setTypeface(null, Typeface.NORMAL) }
+            }
+        }
+
+        serifRD.setOnCheckedChangeListener { _, isChecked ->
+            when(isChecked){
+                true -> { textView.typeface = Typeface.SERIF}
+            }
+        }
+
+        sansRD.setOnCheckedChangeListener { buttonView, isChecked ->
+            when(isChecked){
+                true -> { textView.typeface = Typeface.SANS_SERIF}
+            }
+        }
+        
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                textView.textSize = seekBar?.progress?.toFloat() ?: 10F
+            }
+
+        })
+
     }
 }
